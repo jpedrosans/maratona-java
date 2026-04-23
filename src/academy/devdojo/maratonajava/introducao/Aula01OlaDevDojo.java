3387c6e0f01980235d70b18704bbf253751c5a9f
jpedrosans <jpedrosousans@gmail.com>
2026-04-23 19:34:45 -0300

    Aula 01 - Tipos De Comentários

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Comentário <b>javadoc</b>
+ */
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+
+        // Comentário de uma linha
+
+        /*
+            Comentário
+            de múltiplas
+            linhas
+         */
+    }
+}
