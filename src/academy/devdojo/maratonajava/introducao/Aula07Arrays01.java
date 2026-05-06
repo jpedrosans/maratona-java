a2f88f7ecdf97b21af2b8f0b2346bf7caa8ad2b0
João Pedro <jpedrosousans@gmail.com>
2026-05-06 20:49:14 -0300

    Aula 07 - Arrays / INICIALIZAÇÕES DE ARRAYS / FOREACH

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 0;
+        idades[1] = 1;
+        idades[2] = 2;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float e double = 0
+        //char '\u0000' = ' '
+        //boolean  = false
+        //String = null
+        String[] nomes = new String[3];
+        nomes[0] = "João";
+        nomes[1] = "Ana";
+        nomes[2] = "Pedro";
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
